1141188902a58aed6556668edf71c1ddb63658d8
Vinicius Oliveira <viniciusjesus16@gmail.com>
2022-08-18 13:47:56 -0300

    Adding pipes and their animation

--- game.js
+++ game.js
@@ -26,14 +26,15 @@ class spriteObj
         this.distanceFromTop = distanceFromTop;
     }
 
-    render(renderingSecond = false) {
+    render(renderingSecond = false, distanceFromTop) {
         const distanceFromLeft = this.distanceFromLeft + (renderingSecond ? this.width : 0);
+        distanceFromTop = distanceFromTop || this.distanceFromTop;
         
         canvasContext.drawImage(
             sprites,
             this.x, this.y,
             this.width, this.height,
-            distanceFromLeft, this.distanceFromTop,
+            distanceFromLeft, distanceFromTop,
             this.width, this.height
         );
     }
@@ -45,7 +46,7 @@ class FlappyBird extends spriteObj
         super(x, y, width, height, distanceFromLeft, distanceFromTop);
         
         this.baseDistanceFromTop = distanceFromTop;
-        this.gravity = 0.25;
+        this.gravity = 0.225;
         this.velocity = 0;
         this.jumpHeight = 4.6;
 
@@ -61,6 +62,7 @@ class FlappyBird extends spriteObj
     update() {
         if (this.crashOnGround()) {
             hit_sound.play();
+            this.distanceFromTop = ground.distanceFromTop - this.height;
             changeScreen(endScreen);
         }
 
@@ -93,7 +95,7 @@ class Ground extends spriteObj
     constructor(x, y, width, height, distanceFromLeft, distanceFromTop) {
         super(x, y, width, height, distanceFromLeft, distanceFromTop);
         
-        this.velocity = 1;
+        this.velocity = 2;
     }
 
     update() {
@@ -104,7 +106,53 @@ class Ground extends spriteObj
     }
 }
 
-var flappyBird, ground, background, startGame, endGame;
+class Pipes extends spriteObj
+{
+    constructor(topX, topY, bottomX, bottomY, width, height, distanceFromLeft) {
+        super();
+
+        this.width = width;
+        this.height = height;
+        this.distanceFromLeft = distanceFromLeft;
+
+        this.topX = topX;
+        this.topY = topY;
+        this.bottomX = bottomX;
+        this.bottomY = bottomY;
+
+        this.distanceBetweenPipes = 100;
+        this.velocity = 2;
+
+        this.changePosition();
+    }
+
+    render() {
+        console.log(this.distanceFromTop);
+        this.x = this.topX;
+        this.y = this.topY;
+        
+        super.render();
+
+        this.x = this.bottomX;
+        this.y = this.bottomY;
+        const secondPipeDistanceFromTop = this.height + this.distanceFromTop + this.distanceBetweenPipes;
+        
+        super.render(false, secondPipeDistanceFromTop);
+    }
+
+    update() {
+        if (this.distanceFromLeft <= -50) {
+            this.distanceFromLeft = canvas.offsetWidth + 50;
+            this.changePosition();
+        }
+            
+        this.distanceFromLeft -= this.velocity;
+    }
+
+    changePosition = () => this.distanceFromTop = -175 * (Math.random() + 1);
+}
+
+var flappyBird, ground, background, startGame, endGame, pipe1, pipe2;
 
 function CreateInstances() {
     flappyBird = new FlappyBird(0, 0, 34, 24, 45, canvas.offsetHeight / 3);
@@ -112,9 +160,13 @@ function CreateInstances() {
     background = new spriteObj(390, 0, 276, 204, 0, canvas.offsetHeight - 204);
     startGame = new spriteObj(134, 0, 174, 152, canvas.offsetWidth / 2 - 87, canvas.offsetHeight / 5);
     endGame = new spriteObj(134, 153, 226, 200, canvas.offsetWidth / 2 - 113, canvas.offsetHeight / 5);
+    pipe1 = new Pipes(52, 169, 0, 169, 52, 400, canvas.offsetWidth + 50, -200);
+    pipe2 = new Pipes(52, 169, 0, 169, 52, 400, canvas.offsetWidth + 250, -200);
 }
 
 CreateInstances();
+
+const renderPipes = () => [pipe1, pipe2].forEach(pipe => { pipe.render(); pipe.update(); });
 
 class Screen
 {
@@ -126,12 +178,14 @@ class Screen
         background.render(true);
 
         flappyBird.render();
+
+        [pipe1, pipe2].forEach(pipe => { pipe.render(); })
         
         ground.render();
         ground.render(true);
     }
-
-    update() {}
+    
+    initialize() {}
     click() {}
 }
 
@@ -157,12 +211,10 @@ class GameScreen extends Screen
 {
     render() {
         super.render();
-        ground.update();
-    }
-
-    update() {
+        [pipe1, pipe2].forEach(pipe => pipe.update());
         flappyBird.update();
         flappyBird.animation();
+        ground.update();
     }
 
     click() {
@@ -190,15 +242,11 @@ let activeScreen = startScreen;
 
 function changeScreen(screen) {
     activeScreen = screen;
-
-    if (screen.initialize)
-        screen.initialize();
+    screen.initialize();
 }
 
 function gameLoop() {
     activeScreen.render();
-    activeScreen.update();
-    
     actualFrame++;
     requestAnimationFrame(gameLoop);
 }
